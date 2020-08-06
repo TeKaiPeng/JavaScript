86dfb695403500fffec6d1d44568fbea8f7b13c9
steven <steven@tenlong.com.tw>
2020-08-06 16:51:41 +0800

    JS

--- pastleo-js-lesson-master/05_timer-jquery/main.js
+++ pastleo-js-lesson-master/05_timer-jquery/main.js
@@ -2,13 +2,16 @@ $(document).ready(function() {
     let cnt = 0, running = false, interval;
     $('button').click(function() {
         if (running){
+            $('#running').slideUp();
             clearInterval(interval)
             $('button').text('start')
             running = false
         } else {
+            $('#running').slideDown();
             interval = setInterval(function(){
                 cnt = cnt + 1
                 $('#seconds').text(cnt)
+                $('.dots div:last-child').prependTo($('.dots'))
             }, 100)
             $('button').text('stop')
             running = true
